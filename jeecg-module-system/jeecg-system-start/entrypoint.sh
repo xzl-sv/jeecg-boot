2c12bab033c753a4c4076f290dd51f77f155e29b
qianshihua <qianshihua@zailingtech.com>
2024-04-17 09:13:20 +0800

    黑名单导入失败的处理，内存调整为3G

--- jeecg-module-system/jeecg-system-start/entrypoint.sh
+++ jeecg-module-system/jeecg-system-start/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec java  -Djava.security.egd=file:/dev/./urandom   -jar   /jeecg-boot/jeecg-system-start-3.6.2.jar    --spring.config.location=file:/jeecg-boot/application-dev.yml
+exec java -Xmx3G -Xms3G  -Djava.security.egd=file:/dev/./urandom   -jar   /jeecg-boot/jeecg-system-start-3.6.2.jar    --spring.config.location=file:/jeecg-boot/application-dev.yml
